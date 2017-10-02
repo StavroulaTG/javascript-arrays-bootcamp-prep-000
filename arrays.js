6e328e5221097d679dce5796cd9341516abbab27
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-02 16:08:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,6 @@ function addElementToBeginningOfArray (chocolateBars){
   return ['foo', ...chocolateBars]
 }
 function destructivelyAddElementToBeginningOfArray (chocolateBars){
-  chocolateBars.push(1, 1)
+  chocolateBars.push('foo')
   return chocolateBars
 }
