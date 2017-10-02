d8514114dbc0c3643d6e88a812eed051f9df0a5a
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-02 18:32:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,6 +33,7 @@ function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
   return chocolateBars
 }
 function removeElementFromEndOfArray (chocolateBars) {
+return
 chocolateBars = chocolateBars.slice (0, chocolateBars - 1)
-return chocolateBars
+
 }
