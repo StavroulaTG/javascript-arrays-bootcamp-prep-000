3a9c34b1b35c9315e206be2b685c727204ba95a3
Stavroula Guerrero <4stacyg@optonline.net>
2017-10-02 15:53:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray (chocolateBars){
   return ['foo', ...chocolateBars]
 }
+function destructivelyAddElementToBeginningOfArray (chocolateBars){
+  return ['foo', ...chocolateBars]
+}
